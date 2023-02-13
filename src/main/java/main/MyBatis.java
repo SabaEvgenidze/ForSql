20e2358230fda3accd1b8c70b5c23b78f47da829
Saba <121631242+SabaEvgenidze@users.noreply.github.com>
2023-02-13 13:56:37 +0400

    hello

--- src/main/java/main/MyBatis.java
+++ src/main/java/main/MyBatis.java
@@ -1,6 +1,7 @@
 package main;
 
 import dao.ICashierDao;
+import models.Cashier;
 import mybatis.CashierServiceImpl;
 import org.apache.log4j.Logger;
 
@@ -8,8 +9,12 @@ import java.sql.SQLException;
 
 public class MyBatis {
     private static final Logger LOGGER = Logger.getLogger(MyBatis.class);
-    public static void main(String [] argv) throws SQLException {
+
+    public static void main(String[] argv) throws SQLException {
         ICashierDao cashierDao = new CashierServiceImpl();
         LOGGER.info(cashierDao.selectEntityById(2L));
+
+        Cashier object = new Cashier(6L, 1L);
+        cashierDao.insertEntity(object);
     }
 }
